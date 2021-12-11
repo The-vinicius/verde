c160b015d9d56411c51cf50bf517ed513447176a
The-vinicius <viniciusnao41@gmail.com>
2021-12-11 09:00:50 +0000

    insert STATICFILES_DIRS in green/settings.py

--- green/settings.py
+++ green/settings.py
@@ -123,6 +123,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# arquivos estaticos locais
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, "static_local")
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
