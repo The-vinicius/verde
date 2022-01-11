d8e1c484b40af92f7812ce8152fa0c023d9c0a50
The-vinicius <viniciusnao41@gmail.com>
2022-01-11 09:37:29 +0000

    register CustomUser model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class UserAdmin(auth_admin.UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username',]
