4fe2adf5b8a2e7a66eb61f4d41d24ae7ab042277
The-vinicius <viniciusnao41@gmail.com>
2022-01-12 07:21:04 +0000

    add audit_log

--- green/settings.py
+++ green/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'audit_log.middleware.UserLoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'green.urls'
