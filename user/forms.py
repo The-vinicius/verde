a6a6e034e421a0b2352fdc574c80a04db3b9ae5d
The-vinicius <viniciusnao41@gmail.com>
2022-01-12 06:33:37 +0000

    inserted fields

--- user/forms.py
+++ user/forms.py
@@ -3,6 +3,11 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    
+    username = forms.CharField(label='username', min_length=5, max_length=150)  
+    email = forms.EmailField(label='email')  
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)  
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput) 
 
     class Meta:
         model = CustomUser
