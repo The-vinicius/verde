dabe8c9df95526bcb9424f3ed23e86cd63b02fb6
The-vinicius <viniciusnao41@gmail.com>
2022-01-16 11:17:33 +0000

    created views EventListView and EventDetailSlugView

--- events/views.py
+++ events/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Event
+
+
+class EventListView(ListView):
+	queryset = Event.objects.all() 
+	template_name = "events/events_list.html"
+
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(EventListView, self).get_context_data(*args, **kwargs)
+		print(context)
+		return context
+
+class EventDetailSlugView(DetailView):
+	template_name = "events/events_detail.html"
