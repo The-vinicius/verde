3100b6a8c1f3f451fcbef56c761d85a7b160d0ea
The-vinicius <viniciusnao41@gmail.com>
2022-01-31 21:32:55 +0000

    total of participants in context. created function post for call view add_ùser_event. created view add_user_event for add a participants.

--- events/views.py
+++ events/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import ListView, DetailView
-from django.http import Http404
+from django.shortcuts import redirect, render 
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from .models import Event
+from user.models import CustomUser
+from django.utils.http import url_has_allowed_host_and_scheme
 
 
 class EventListView(ListView):
@@ -19,6 +22,9 @@ class EventDetailSlugView(DetailView):
 
 	def get_context_data(self, *args, **kwargs):
 		context = super(EventDetailSlugView, self).get_context_data(*args, **kwargs)
+		slug = self.kwargs.get('slug')
+		qs = self.get_object()
+		context['total'] = qs.participantes.count()
 		return context
 
 	def get_object(self, *args, **kwargs):
@@ -32,3 +38,32 @@ class EventDetailSlugView(DetailView):
 			qs = Event.objects.filter(slug = slug)
 			instace = qs.first()
 		return instace
+
+
+	def post(self, request, *args, **kwargs):
+		if request.user.is_authenticated:
+			return add_user_event(request)
+		else:
+			return redirect('/accounts/login/')
+
+def add_user_event(request):
+	slug = request.POST.get('slug')
+	user_id = request.POST.get('user_id')
+
+	#url redirect
+	next_post = request.POST.get('next')
+	next_get = request.GET.get('next')
+	redirect_path = next_get or next_post or None
+
+	if user_id is not None:
+		try:
+			user_entry = CustomUser.objects.get(id=user_id)
+		except CustomUser.DoesNotExist:
+			redirect('/login/')
+		else:
+			qs = Event.objects.get(slug=slug)
+			qs.participantes.add(user_entry)
+		if url_has_allowed_host_and_scheme(redirect_path, request.get_host()):
+			return redirect(redirect_path)
+
+	return Redirect('/events/')
