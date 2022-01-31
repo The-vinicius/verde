1addcd14ac4ed2e4dc578f790bdcee219b87f07d
The-vinicius <viniciusnao41@gmail.com>
2022-01-31 21:33:51 +0000

    add url add_user

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import EventDetailSlugView, EventListView
+from .views import EventDetailSlugView, EventListView, add_user_event
 
 
 app_name = 'events'
@@ -7,5 +7,6 @@ app_name = 'events'
 
 urlpatterns = [
 	path('', EventListView.as_view(), name='list'),
-	path('<slug:slug>/', EventDetailSlugView.as_view(), name='detail')
+	path('<slug:slug>/', EventDetailSlugView.as_view(), name='detail'),
+	path('add_user/', add_user_event, name='add_user')
 	]
