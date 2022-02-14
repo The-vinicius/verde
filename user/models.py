b711114c95a18368b2c26693861439357ad81010
The-vinicius <viniciusnao41@gmail.com>
2022-02-14 16:31:16 +0000

    import model Event, create a UserQuerySet for get event with user

--- user/models.py
+++ user/models.py
@@ -1,9 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from events.models import Event
+
+class UserQuerySet(models.query.QuerySet):
+    def events_user(self, user):
+        return Event.objects.filter(participantes=user)
+
+class UserManager(models.Manager):
+    def get_queryset(self):
+        return UserQuerySet(self.model, using=self._db)
+
+    def event_set(self, user):
+        return self.get_queryset().events_user(user)
+
 
 class CustomUser(AbstractUser):
-    pass
-    # add additional fields in here
+    objects = UserManager()
+
 
     def __str__(self):
         return self.username
