9a77cf3f7ed1abb52864d1d8e8b1ddab0f1cdb4a
The-vinicius <viniciusnao41@gmail.com>
2022-02-22 19:11:28 +0000

    import UserImageForm and created view UserProfile

--- user/views.py
+++ user/views.py
@@ -1,9 +1,14 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, UserImageForm
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class UserProfile(CreateView):
+    form_class = UserImageForm
+    template_name = 'profile/profile.html'
