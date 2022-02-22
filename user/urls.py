55d724beb925d07ce26fd9db31f5e9d7966fa930
The-vinicius <viniciusnao41@gmail.com>
2022-02-22 19:08:55 +0000

    insert path url profile

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, UserProfile
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('profile/', UserProfile.as_view(), name='profile')
 ]
