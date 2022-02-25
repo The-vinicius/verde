2f8d981bfc3f9728b32cbcf2817f1bbcc78b3e5b
The-vinicius <viniciusnao41@gmail.com>
2022-02-25 19:00:49 +0000

    create userprofile model with fields user and image. delete field image of customuser.

--- user/models.py
+++ user/models.py
@@ -13,11 +13,19 @@ class UserManager(UserManager):
     def event_set(self, user):
         return self.get_queryset().events_user(user)
 
-
+# custom user
 class CustomUser(AbstractUser):
     objects = UserManager()
-    image = models.ImageField(upload_to='user/img/', null = True, blank = True)
 
 
     def __str__(self):
         return self.username
+
+
+
+
+# user profile
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField(upload_to='user/img/', null=True, blank=True)
