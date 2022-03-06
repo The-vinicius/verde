2d3f85d7eecdb8ef245c1c3c787e8501797e141b
The-vinicius <viniciusnao41@gmail.com>
2022-03-06 15:23:28 +0000

    url profile/<str:name>/

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import SignUpView, UserProfile
+from .views import SignUpView, user_profile_view
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('profile/', UserProfile.as_view(), name='profile')
+    path('profile/<str:name>/', user_profile_view, name='profile')
 ]
