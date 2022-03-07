0e435941d658c3d9cf3ced6c25d6e32507403c06
The-vinicius <viniciusnao41@gmail.com>
2022-03-07 09:06:16 +0000

    view user_profile_view get profile, verify if method is post and after form is valid and save image in profile.

--- user/views.py
+++ user/views.py
@@ -1,7 +1,11 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.http import HttpResponse
+from django.shortcuts import render
 
 from .forms import CustomUserCreationForm, UserImageForm
+from .models import UserProfile
+
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
@@ -9,6 +13,21 @@ class SignUpView(CreateView):
     template_name = 'registration/signup.html'
 
 
-class UserProfile(CreateView):
-    form_class = UserImageForm
-    template_name = 'profile/profile.html'
+def user_profile_view(request, name):
+    profile = UserProfile.objects.get_or_create(request)
+    if request.method == 'POST':
+
+        form = UserImageForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            profile[0].image = form.cleaned_data['image']
+            profile[0].save()
+    else:
+        form = UserImageForm()
+
+    context = {
+            'form':form,
+            'profile': profile[0]
+    }
+
+    return render(request, 'profile/profile.html', context)
