2fc61674d2bcd2c4aed416cd6f7d381a1f8e830d
The-vinicius <viniciusnao41@gmail.com>
2022-03-10 20:08:30 +0000

    UserProfileView

--- user/views.py
+++ user/views.py
@@ -1,10 +1,14 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.views.generic import View
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
 from .forms import CustomUserCreationForm, UserImageForm
 from .models import UserProfile
+User = get_user_model()
 
 
 class SignUpView(CreateView):
@@ -13,6 +17,23 @@ class SignUpView(CreateView):
     template_name = 'registration/signup.html'
 
 
+
+class UserProfileView(View):
+    def get(self,request, username, *args, **kwargs):
+        user = get_object_or_404(User, username=username)
+        profile = UserProfile.objects.get(user=user)
+
+        context = {
+                'user': user,
+                'profile':profile
+        }
+
+        return render(request, 'profile/detail.html', context)
+
+
+
+
+@login_required
 def user_profile_view(request, name):
     profile = UserProfile.objects.get_or_new(request)
     
