f29bc356e7309f8d8130c2fafaf6acba70f29c11
The-vinicius <viniciusnao41@gmail.com>
2022-03-10 20:08:57 +0000

    add url profile/view/<str:username>/

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SignUpView, user_profile_view
+from .views import SignUpView, user_profile_view, UserProfileView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('profile/<str:name>/', user_profile_view, name='profile')
+    path('profile/view/<str:username>/', UserProfileView.as_view(), name='profile'),
+    path('profile/edit/<str:name>/', user_profile_view, name='edit-profile')
 ]
