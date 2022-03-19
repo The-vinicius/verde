ae17a29a5c0ac04ffc223d95a83c877286a364cc
The-vinicius <viniciusnao41@gmail.com>
2022-03-19 10:40:29 +0000

    remove UserImageForm

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm, UserImageForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser, UserProfile
 
 
