9747aff806a37a55a9018d094182db32d40d5d25
The-vinicius <viniciusnao41@gmail.com>
2022-03-31 10:04:45 +0000

    new forms.

--- user/views.py
+++ user/views.py
@@ -2,11 +2,11 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.views.generic import View
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 
-from .forms import CustomUserCreationForm, UserImageForm
+from .forms import CustomUserCreationForm, EditProfileForm
 from .models import UserProfile
 User = get_user_model()
 
@@ -34,22 +34,29 @@ class UserProfileView(View):
 
 
 @login_required
-def user_profile_view(request, name):
-    profile = UserProfile.objects.get_or_new(request)
-    
-    if request.method == 'POST':
+def edit_profile(request):
+    user = request.user.id
+    profile = UserProfile.objects.get(user__id=user)
 
-        form = UserImageForm(request.POST, request.FILES)
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, request.FILES, instance=profile)
 
         if form.is_valid():
-            profile.image = form.cleaned_data['image']
-            profile.save()
+            profile.image = form.cleaned_data.get('image')
+            profile.bio   = form.cleaned_data.get('bio')
+            profile.gender = form.cleaned_data.get('gender')
+            profile.wallet = form.cleaned_data.get('wallet')
+            profile.save() 
+
+            return redirect('accounts:profile', username=request.user.username)
     else:
-        form = UserImageForm()
+        form = EditProfileForm(instance=profile)
+
 
     context = {
-            'form':form,
-            'profile': profile
+        'form': form,
+        'profile':profile
     }
+
 
     return render(request, 'profile/profile.html', context)
