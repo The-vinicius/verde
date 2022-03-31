d005c196932e6e255c8478bc91161b72a5132314
The-vinicius <viniciusnao41@gmail.com>
2022-03-31 10:03:59 +0000

    update url edit-profile

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import SignUpView, user_profile_view, UserProfileView
+from .views import SignUpView, edit_profile, UserProfileView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('profile/view/<str:username>/', UserProfileView.as_view(), name='profile'),
-    path('profile/edit/<str:name>/', user_profile_view, name='edit-profile')
+    path('profile/edit/', edit_profile, name='edit-profile')
 ]
